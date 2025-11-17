662d35f44909ae3e37166266b64a3efa0d70516b
HippomasAKiB1 <akibhasankp1245@gmail.com>
2025-11-17 16:19:21 +0600

    addition feature added

--- lab4(calculator)/script.js
+++ lab4(calculator)/script.js
@@ -7,15 +7,6 @@ calc.style.padding = "10px";
 calc.style.border = "1px solid #000";
 app.appendChild(calc);
 
-let powerOn = false; 
-
-const powerBtn = document.createElement("button");
-powerBtn.textContent = "OFF";
-powerBtn.style.width = "100%";
-powerBtn.style.height = "40px";
-powerBtn.style.margin = "5px 0";
-calc.appendChild(powerBtn);
-
 
 const display = document.createElement("input");
 display.disabled = true;
@@ -24,6 +15,16 @@ display.style.height = "40px";
 display.style.fontSize = "20px";
 display.style.textAlign = "right";
 calc.appendChild(display);
+
+
+let powerOn = false;
+
+const powerBtn = document.createElement("button");
+powerBtn.textContent = "OFF";
+powerBtn.style.width = "100%";
+powerBtn.style.height = "40px";
+powerBtn.style.margin = "5px 0";
+calc.appendChild(powerBtn);
 
 
 const nums = ["7","8","9","4","5","6","1","2","3","0"];
@@ -46,8 +47,29 @@ nums.forEach(n => {
 
     numButtons.push(btn);
     calc.appendChild(btn);
-
 });
+
+
+let firstNumber = null;
+let operator = null;
+
+const plusBtn = document.createElement("button");
+plusBtn.textContent = "+";
+plusBtn.style.width = "45px";
+plusBtn.style.height = "45px";
+plusBtn.style.margin = "3px";
+plusBtn.disabled = true;
+calc.appendChild(plusBtn);
+
+
+const equalBtn = document.createElement("button");
+equalBtn.textContent = "=";
+equalBtn.style.width = "100%";
+equalBtn.style.height = "45px";
+equalBtn.style.margin = "5px 0";
+equalBtn.disabled = true;
+calc.appendChild(equalBtn);
+
 
 powerBtn.addEventListener("click", () => {
     powerOn = !powerOn;
@@ -55,13 +77,42 @@ powerBtn.addEventListener("click", () => {
     if (powerOn) {
         powerBtn.textContent = "ON";
         display.disabled = false;
+
         numButtons.forEach(b => b.disabled = false);
+        plusBtn.disabled = false;
+        equalBtn.disabled = false;
+
     } else {
         powerBtn.textContent = "OFF";
         display.disabled = true;
         display.value = "";
+
         numButtons.forEach(b => b.disabled = true);
+        plusBtn.disabled = true;
+        equalBtn.disabled = true;
+
+        firstNumber = null;
+        operator = null;
     }
 });
 
 
+plusBtn.addEventListener("click", () => {
+    if (!powerOn) return;
+    if (display.value === "") return;
+
+    firstNumber = Number(display.value);
+    operator = "+";
+    display.value = "";
+});
+
+
+equalBtn.addEventListener("click", () => {
+    if (!powerOn) return;
+    if (operator === "+" && display.value !== "") {
+        const secondNumber = Number(display.value);
+        const result = firstNumber + secondNumber;
+        display.value = result;
+        operator = null;
+    }
+});
